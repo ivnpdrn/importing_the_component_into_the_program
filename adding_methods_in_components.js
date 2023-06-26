0514a669e70cac7e1382edfd9fe0b07724c99432
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-06-25 20:26:09 -0400

    Add files via upload

---
+++ adding_methods_in_components.js
@@ -0,0 +1,48 @@
+const Counter = {
+
+    data () {
+        return {
+            count: 0
+        }
+    },
+
+    template : `{{time()}} &nbsp;&nbsp; 
+    
+    The counter is: {{count}}`,
+
+
+    created() {
+
+        setInterval(() => {
+            this.count += 1;
+        }, 1000)
+    },
+
+    methods : {
+
+        time () {
+
+            // return time as HH:MM:SS
+
+            var date = new Date();
+            var hour = date.getHours();
+            var min = date.getMinutes();
+            var sec = date.getSeconds();
+
+            if (hour < 10) hour = "0" + hour;
+            if (min < 10) min = "0" + min;
+            if (sec < 10) sec = "0" + sec;
+
+            return "" + hour + ":" + min + ":" + sec + "";
+
+        }
+    }
+
+}
+
+export default Counter;
+
+// the time() method is defined in the section methods section and 
+// is then directly used in the component template within the doube braces {{ and }}
+// A method defined in the methods section can use the other methods of this section
+// or the reactive variables of the data section by prefixing then with the this keyword
