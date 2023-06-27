a18d46d95c98dc37e9476cf42da2461e0fdb0020
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-06-27 12:57:31 -0400

    Add files via upload

---
+++ setting_the_start_attribute_in_the_counter_component_sarrion_bk_pg_115_counter.js
@@ -0,0 +1,58 @@
+const Counter = {
+
+    data () {
+        return {
+            count: 0
+        }
+    },
+
+    template : `{{time()}} &nbsp;&nbsp;
+    
+    
+    The counter is: {{count}}, double is:  {{countX2}}`,
+
+
+    created() {
+
+        setInterval(() => {
+            this.count += 1;
+        }, 1000)
+    },
+
+    methods : {
+
+        time () {
+
+            // return time as HH:MM:SS
+
+            var date = new Date();
+            var hour = date.getHours();
+            var min = date.getMinutes();
+            var sec = date.getSeconds();
+
+            if (hour < 10) hour = "0" + hour;
+            if (min < 10) min = "0" + min;
+            if (sec < 10) sec = "0" + sec;
+
+            return "" + hour + ":" + min + ":" + sec + "";
+
+        }
+    },
+
+    computed : {
+        countX2 () {
+            return 2 * this.count;
+        }
+    }
+
+}
+
+export default Counter;
+
+// the time() method is defined in the section methods section and 
+// is then directly used in the component template within the doube braces {{ and }}
+// A method defined in the methods section can use the other methods of this section
+// or the reactive variables of the data section by prefixing then with the this keyword
+
+// defining computed properties in the computed section:
+// Every second leads to the automatic modification of the countX2 variable thanks to its definition in the computed section.
