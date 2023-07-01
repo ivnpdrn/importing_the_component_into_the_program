6bdb7e7bca632c1f3dc0412b217fec33adafc34e
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-06-30 22:17:44 -0400

    Add files via upload

--- ver3_displaying_counters_and_their_index_counter_sarrion_bk_pg_121.js
+++ ver3_displaying_counters_and_their_index_counter_sarrion_bk_pg_121.js
@@ -3,7 +3,7 @@ const Counter = {
 
     data()  {
         return {
-            counts : ["Counter 1", "Counter 2", "Counter 3", "Counter 4", "Counter 5","Counter 6", "Counter7"]
+            counts : ["Counter 1", "Counter 2", "Counter 3", "Counter 4", "Counter 5","Counter 6", "Counter 7"]
 
         }
 
