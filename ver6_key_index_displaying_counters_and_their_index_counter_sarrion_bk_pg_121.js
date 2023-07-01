cdad0032df2152b2429074137215e80400c3963e
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-06-30 23:06:58 -0400

    Add files via upload

---
+++ ver6_key_index_displaying_counters_and_their_index_counter_sarrion_bk_pg_121.js
@@ -0,0 +1,33 @@
+
+const Counter = {
+
+    data()  {
+        return {
+            counts : ["Counter 1", "Counter 2", "Counter 3", "Counter 4", "Counter 5","Counter 6", "Counter 7", "Counter 8", "Counter 9"]
+
+        }
+
+    },
+
+    template : `
+    
+    <ul>
+    
+        <li v-for="(count, index) in counts" :key="index">
+            <span>Index {{index}} : {{count}}</span>
+
+        </li>
+
+    </ul>
+
+    `,
+
+}
+
+export default Counter;
+
+
+// v-for directive on the element that we want to repeat (in this case, the <li> element).
+// the value associated with the v-for directive is a character string of the form "count in counts" 
+// knowing that counts is the variable on which we are iterating.
+// the count variable thus corresponds to each of the elements of the countss array.
