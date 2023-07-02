e745902750810f943d6e4dbfa0c9f3f58243fa19
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-01 21:43:30 -0400

    Add files via upload

--- increment_counter_count_sarrion_bk_pg_128.js
+++ increment_counter_count_sarrion_bk_pg_128.js
@@ -23,3 +23,8 @@ const Counter = {
 }
 
 export default Counter;
+
+// it is possible to write several method calls in row during the processing to be performed (separated by a comma or a semicolon).
+// It is enough that these methods are defined in the methods section of the component.
+
+// for example, @click="incr(); incr()" allows the incr() method to be executed twice each time the button is clicked.
