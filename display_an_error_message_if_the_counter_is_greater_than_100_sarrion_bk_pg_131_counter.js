a0982ea2b4cc65b7cc5dfd00002dbd9a2f98c970
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-02 21:06:02 -0400

    Add files via upload

---
+++ display_an_error_message_if_the_counter_is_greater_than_100_sarrion_bk_pg_131_counter.js
@@ -0,0 +1,36 @@
+
+
+const Counter = {
+    data ()  {
+        return {
+            count : 0,
+            message : ""
+
+        }
+        
+    },
+
+    template : `
+        count (less than 100) : <input type="text" : value="count" @blur="valid($event)" />
+        &nbsp; &nbsp; count = {{count}}
+        <br><br>
+        <span>{{message}}</span>
+        
+    `,
+
+    methods   :  {
+        valid(event) {
+            this.message = "";      // reset of the error message
+                                    // before each check
+            if (event.target.value < 100) this.count = event.target.value;
+            else this.message = "Error: count must be less than 100";
+
+        }
+    }
+}
+
+export default Counter;
+
+// The $event parameter is passed processing function.
+// The event.target property provides direct access to the HTML element
+// Its value property contains the value of the field 
